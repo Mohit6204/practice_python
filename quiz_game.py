f146d2a84d28783d7d45ab1b39298f25b655d1ff
Mohit Kumar Mishra <mishramohit6204@gmail.com>
2024-04-22 20:37:10 +0530

    created a simple quiz game using python

---
+++ quiz_game.py
@@ -0,0 +1,11 @@
+questions={
+    "Who created Python?: ":"A",
+    "What year was Python created?:":"B",
+    "Python is tributed to which comedy group?: ":"C",
+    "Is the Earth round?: ":"A"
+}
+options=[["A. Guido van Rossum","B.Elon Musk","C. Bill Gates","D. Mark Zukerberg"],
+         ["A. 1989","B. 1991","C. 2000","D. 2010"],
+         ["A. Lonely Island", "B. Smosh", "C. Monty Python","D. SNL"],
+         ["A. True","B. False","C. Sometimes","D. What's Earth?"]]
+
